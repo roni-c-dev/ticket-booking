f43bd7c9e7239241438c9eb6bad7a5be2b640d87
roni-c-dev <107492068+roni-c-dev@users.noreply.github.com>
2022-12-10 14:19:07 +0000

    add api interaction

--- cinema-tickets-javascript/src/api/index.js
+++ cinema-tickets-javascript/src/api/index.js
@@ -1,16 +1,28 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 import TicketService from "../pairtest/TicketService.js";
+import TicketTypeRequest from "../pairtest/lib/TicketTypeRequest.js";
 import * as testdata from "../../test/testdata.js";
 
+// allow the app to use request body
+app.use(express.json());
+
+app.use(cors({
+  // requests from our front end are ok (anywhere else seems sketchy)
+  origin: 'http://localhost:1234'
+}));
 
 const port = process.env.port || 8080;
-app.get("/", function (req, res) { 
+
+// Under construction - currently handling only one ticket request at a time for testing purposes
+app.post("/", function (req, res) { 
     try {
+      let newTicketReq = new TicketTypeRequest(req.body.tickettype, parseInt(req.body.ticketcount));
       res.send({
-        response: new TicketService().purchaseTickets(parseInt(req.headers.accountid),[testdata.requestAdult])
+        response: new TicketService().purchaseTickets(parseInt(req.body.accountid),[newTicketReq])
       })
     } catch (err) {
       res.status(500).send({
@@ -19,6 +31,8 @@ app.get("/", function (req, res) {
     }
     
 })
+
+
   
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`)
